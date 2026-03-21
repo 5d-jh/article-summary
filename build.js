212a324b0b58a6aa00cf299a5583199782f7f1a2
5d-jh <jeonghyen0126@gmail.com>
2026-03-21 14:58:05 +0900

    Fix build script

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
 const esbuild = require('esbuild');
 const fs = require('fs');
 const path = require('path');
-const { execSync } = require('child_process');
 const pkg = require('./package.json');
+const sharp = require('sharp');
 
 const outDirChrome = path.join(__dirname, 'dist', 'chrome');
 const outDirFirefox = path.join(__dirname, 'dist', 'firefox');
@@ -23,109 +23,113 @@ esbuild.buildSync({
     target: ['es2020'],
 });
 
-// Generate icons from SVG using macs native sips tool
-const iconSizes = [16, 32, 48, 128];
-iconSizes.forEach(size => {
-    console.log(`Generating ${size}x${size} icon...`);
-    execSync(`sips -s format png -z ${size} ${size} icon.svg --out icon${size}.png`);
-});
+async function build() {
+    // Generate icons from SVG using sharp
+    const iconSizes = [16, 32, 48, 128];
+    for (const size of iconSizes) {
+        console.log(`Generating ${size}x${size} icon...`);
+        await sharp('icon.svg').resize(size, size).png().toFile(`icon${size}.png`);
+    }
 
-// Generate dark-mode icons (bright version for dark backgrounds)
-iconSizes.forEach(size => {
-    console.log(`Generating ${size}x${size} dark-mode icon...`);
-    execSync(`sips -s format png -z ${size} ${size} icon-dark.svg --out icon${size}-dark.png`);
-});
+    // Generate dark-mode icons (bright version for dark backgrounds)
+    for (const size of iconSizes) {
+        console.log(`Generating ${size}x${size} dark-mode icon...`);
+        await sharp('icon-dark.svg').resize(size, size).png().toFile(`icon${size}-dark.png`);
+    }
 
-// Copy assets and dist/temp to both browsers
-const assets = ['src/options.html', 'src/popup.html', 'src/styles.css', 'icon16.png', 'icon32.png', 'icon48.png', 'icon128.png', 'icon16-dark.png', 'icon32-dark.png', 'icon48-dark.png', 'icon128-dark.png'];
+    // Copy assets and dist/temp to both browsers
+    const assets = ['src/options.html', 'src/popup.html', 'src/styles.css', 'icon16.png', 'icon32.png', 'icon48.png', 'icon128.png', 'icon16-dark.png', 'icon32-dark.png', 'icon48-dark.png', 'icon128-dark.png'];
 
-const manifestBase = {
-    name: "MyGist",
-    version: pkg.version,
-    description: pkg.description,
-    permissions: ["storage", "activeTab"],
-    host_permissions: ["http://*/*", "https://*/*", "<all_urls>"],
-    options_ui: {
-        page: "options.html",
-        open_in_tab: true
-    },
-    action: {
-        default_popup: "popup.html"
-    },
-    content_scripts: [
-        {
-            matches: ["<all_urls>"],
-            js: ["content.js"]
+    const manifestBase = {
+        name: "MyGist",
+        version: pkg.version,
+        description: pkg.description,
+        permissions: ["storage", "activeTab"],
+        host_permissions: ["http://*/*", "https://*/*", "<all_urls>"],
+        options_ui: {
+            page: "options.html",
+            open_in_tab: true
+        },
+        action: {
+            default_popup: "popup.html"
+        },
+        content_scripts: [
+            {
+                matches: ["<all_urls>"],
+                js: ["content.js"]
+            }
+        ],
+        icons: {
+            "16": "icon16.png",
+            "32": "icon32.png",
+            "48": "icon48.png",
+            "128": "icon128.png"
         }
-    ],
-    icons: {
-        "16": "icon16.png",
-        "32": "icon32.png",
-        "48": "icon48.png",
-        "128": "icon128.png"
-    }
-};
+    };
 
-// --- Chrome Manifest (V3) ---
-const manifestChrome = {
-    ...manifestBase,
-    manifest_version: 3,
-    background: {
-        service_worker: "background.js",
-        type: "module"
-    }
-};
+    // --- Chrome Manifest (V3) ---
+    const manifestChrome = {
+        ...manifestBase,
+        manifest_version: 3,
+        background: {
+            service_worker: "background.js",
+            type: "module"
+        }
+    };
 
-// --- Firefox Manifest (V2 or V3) ---
-// Note: Firefox Android works well with V2 for extensions, but V3 is supported from 109+. Let's use V3 with browser_specific_settings.
-const manifestFirefox = {
-    ...manifestBase,
-    manifest_version: 2,
-    browser_action: {
-        default_popup: "popup.html",
-        theme_icons: iconSizes.map(size => ({
-            light: `icon${size}-dark.png`,
-            dark: `icon${size}.png`,
-            size: size
-        })),
-        default_icon: 'icon48.png'
-    },
-    background: {
-        scripts: ["background.js"]
-    },
-    browser_specific_settings: {
-        gecko: {
-            id: "mygist@5d-jh.dev",
-            strict_min_version: "109.0",
-            data_collection_permissions: {
-                required: ["none"]
+    // --- Firefox Manifest (V2 or V3) ---
+    // Note: Firefox Android works well with V2 for extensions, but V3 is supported from 109+. Let's use V3 with browser_specific_settings.
+    const manifestFirefox = {
+        ...manifestBase,
+        manifest_version: 2,
+        browser_action: {
+            default_popup: "popup.html",
+            theme_icons: iconSizes.map(size => ({
+                light: `icon${size}-dark.png`,
+                dark: `icon${size}.png`,
+                size: size
+            })),
+            default_icon: 'icon48.png'
+        },
+        background: {
+            scripts: ["background.js"]
+        },
+        browser_specific_settings: {
+            gecko: {
+                id: "mygist@5d-jh.dev",
+                strict_min_version: "109.0",
+                data_collection_permissions: {
+                    required: ["none"]
+                }
             }
         }
-    }
-};
-// In V2, host_permissions are inside permissions
-manifestFirefox.permissions = [...manifestFirefox.permissions, "<all_urls>", "http://*/*", "https://*/*"];
-delete manifestFirefox.host_permissions;
-delete manifestFirefox.action;
+    };
+    // In V2, host_permissions are inside permissions
+    manifestFirefox.permissions = [...manifestFirefox.permissions, "<all_urls>", "http://*/*", "https://*/*"];
+    delete manifestFirefox.host_permissions;
+    delete manifestFirefox.action;
 
-// Write Manifests
-fs.writeFileSync(path.join(outDirChrome, 'manifest.json'), JSON.stringify(manifestChrome, null, 2));
-fs.writeFileSync(path.join(outDirFirefox, 'manifest.json'), JSON.stringify(manifestFirefox, null, 2));
+    // Write Manifests
+    fs.writeFileSync(path.join(outDirChrome, 'manifest.json'), JSON.stringify(manifestChrome, null, 2));
+    fs.writeFileSync(path.join(outDirFirefox, 'manifest.json'), JSON.stringify(manifestFirefox, null, 2));
 
-// Copy files
-const copyFiles = (srcDir, destDir) => {
-    ['options.js', 'content.js', 'background.js', 'popup.js'].forEach(f => {
-        fs.copyFileSync(path.join(__dirname, 'dist', 'temp', f), path.join(destDir, f));
-    });
-    // ['options.js.map', 'content.js.map', 'background.js.map'].forEach(f => {
-    //     fs.copyFileSync(path.join(__dirname, 'dist', 'temp', f), path.join(destDir, f));
-    // });
-    assets.forEach(f => {
-        fs.copyFileSync(path.join(__dirname, f), path.join(destDir, path.basename(f)));
-    });
-};
+    // Copy files
+    const copyFiles = (srcDir, destDir) => {
+        ['options.js', 'content.js', 'background.js', 'popup.js'].forEach(f => {
+            fs.copyFileSync(path.join(__dirname, 'dist', 'temp', f), path.join(destDir, f));
+        });
+        assets.forEach(f => {
+            fs.copyFileSync(path.join(__dirname, f), path.join(destDir, path.basename(f)));
+        });
+    };
 
-copyFiles('dist/temp', outDirChrome);
-copyFiles('dist/temp', outDirFirefox);
+    copyFiles('dist/temp', outDirChrome);
+    copyFiles('dist/temp', outDirFirefox);
 
-console.log("Build complete! Extensions are in dist/chrome and dist/firefox.");
+    console.log("Build complete! Extensions are in dist/chrome and dist/firefox.");
+}
+
+build().catch(err => {
+    console.error(err);
+    process.exit(1);
+});
